55c4fddcf918771822459ad3efe4f687f8550696
TaotaoXinYu <814546515@qq.com>
2018-07-06 11:23:53 +0800

    Initial commit

---
+++ src/aa.java
@@ -0,0 +1,15 @@
+public class aa {
+    private int a;
+    private int b;
+
+    public aa() {
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
